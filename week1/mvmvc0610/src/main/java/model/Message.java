cba47acb31c09643837b5fa989f64b5ce22dac9b
Vincent Yahoo <zvincentchang@yahoo.com.tw>
2026-06-10 20:49:38 +0800

    Add greeting messages to Message class

--- week1/mvmvc0610/src/main/java/model/Message.java
+++ week1/mvmvc0610/src/main/java/model/Message.java
@@ -1,5 +1,14 @@
 package model;
 
 public class Message {
-
+   Map<String,String> greet=new HashMap<>();
+   public Message() {
+	   greet.put("Danny", "Hello");
+	   greet.put("Mary", "Good Evening");
+	   greet.put("Tony", "Good Day");	   
+   }
+   
+   public String says(String name) {
+	   return name+"said:"+greet.get(name);
+   }
 }
